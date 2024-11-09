749c65b708454cb103e1c56f8514b1dec3f0dc93
Matias Moraga <matimoraga10@gmail.com>
2024-11-09 19:39:35 -0300

    agregamos cuerpo Entrega4

---
+++ Entrega4/microservicio-reporte/src/main/java/MicroservicioReporteApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioReporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioReporteApplication.class, args);
+    }
+
+}
---
+++ Entrega4/microservicio-usuarioCuenta/src/test/java/org/entrega4/microserviciousuariocuenta/MicroservicioUsuarioCuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.entrega4.microserviciousuariocuenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsuarioCuentaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
