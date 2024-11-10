dd827c49a743a38ce241748a708febf5e8df4566
Agustina Quinteros <142256460+Agustinaqui@users.noreply.github.com>
2024-11-10 20:58:57 -0300

    agrego cambios grales

--- Entrega4/microservicio-monopatin/src/main/java/entity/Monopatin.java
+++ Entrega4/microservicio-monopatin/src/main/java/entity/Monopatin.java
@@ -3,19 +3,21 @@
 @NoArgsConstructor
 @AllArgsConstructor
 public class Monopatin{
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer id_monopatin;
-@Column
-private String ubicacionActual;
-@Column
-private Boolean estaActivo;
-@Column
-private Boolean habilitado;
-@Column
-private Double kilometrosTotales;
-@Column
-private Integer tiempo_uso_total;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_monopatin;
+    @Column
+    private String ubicacionActual;
+    @Column
+    private Boolean estaActivo;
+    @Column
+    private Boolean habilitado;
+    @Column
+    private Double kilometrosTotales;
+    @Column
+    private Integer tiempo_uso_total;
 
 //getters y setters
+
+
 }
